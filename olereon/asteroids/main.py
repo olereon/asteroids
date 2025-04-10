b38dce2495d66d3b3846d3b5e939b8432e7e805c
olereon <shyraoleg@outlook.com>
2025-04-10 21:36:04 +0200

    A-3: reload from reserv

--- olereon/asteroids/main.py
+++ olereon/asteroids/main.py
@@ -3,19 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.time.Clock().tick(60)
+        dt = pygame.time.Clock().tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
