c24af6fe40d1b41b96f54b57862f0acdaafb58b9
olereon <shyraoleg@outlook.com>
2025-04-12 00:40:08 +0200

    A-4: working complete version

--- olereon/asteroids/main.py
+++ olereon/asteroids/main.py
@@ -1,30 +1,69 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
 
 def main():
     pygame.init()
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = updatable
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    updatable.add(player)
+    updatable.add(asteroidfield)
+    drawable.add(player) 
+    #drawable.add(asteroidfield)
+    #pygame.display.set_caption("Asteroids")
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        for shot in shots:
+            for asteroid in asteroids:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
-        pygame.time.Clock().tick(60)
-        dt = pygame.time.Clock().tick(60) / 1000
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        #player.draw(screen)
+        pygame.time.Clock().tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
--- olereon/asteroids/player.py
+++ olereon/asteroids/player.py
@@ -1,12 +1,15 @@
 import pygame
-from constants import PLAYER_RADIUS,  PLAYER_TURN_SPEED
+from constants import *
 from circleshape import CircleShape
+from shot import Shot
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.radius = PLAYER_RADIUS
-        
+        self.shoot_timer = 0
+
     # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -22,13 +25,34 @@ class Player(CircleShape):
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-        
+
+
+
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(dt)
             
         elif keys[pygame.K_d]:
             self.rotate(-dt)
+
+        elif keys[pygame.K_w]:
+            self.move(dt)
+        elif keys[pygame.K_s]:
+            self.move(-dt)
+            
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
         
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
